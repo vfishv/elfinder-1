dc89de348046660dcc62a9e2f5da7846d97633b1
Qingtian <quentin@gmail.com>
2021-01-06 16:30:28 +0800

    Update main.go

--- example/main.go
+++ example/main.go
@@ -11,13 +11,13 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./elf/"))))
-	mux.Handle("/connector",http.HandlerFunc(elfHandler))
-	fmt.Println("Listen on :8000")
-	log.Fatal(http.ListenAndServe(":8000", mux))
+	mux.Handle("/connector", http.HandlerFunc(elfHandler))
+	fmt.Println("Listen on :80")
+	log.Fatal(http.ListenAndServe(":00", mux))
 }
 
-func elfHandler(w http.ResponseWriter,r *http.Request){
+func elfHandler(w http.ResponseWriter, r *http.Request) {
 
 	con := elfinder.NewElFinderConnector([]elfinder.Volume{&elfinder.DefaultVolume})
-	con.ServeHTTP(w,r)
+	con.ServeHTTP(w, r)
 }
